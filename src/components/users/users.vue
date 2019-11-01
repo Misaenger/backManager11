1aef50f9f4c79aa2bd0de17a3af87c0e628bbc00
yujun <2785537249@qq.com>
2019-11-01 11:50:57 +0800

    获取用户列表

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -16,18 +16,67 @@
         <el-button type="success">添加用户</el-button>
       </el-col>
     </el-row>
+    <!-- 表格 -->
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column type="index" label="#" width="40">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="80">
+      </el-table-column>
+      <el-table-column prop="address" label="邮箱">
+      </el-table-column>
+      <el-table-column prop="date" label="创建日期">
+      </el-table-column>
+      <el-table-column prop="address" label="用户状态">
+      </el-table-column>
+      <el-table-column prop="address" label="操作">
+      </el-table-column>
+    </el-table>
 
   </el-card>
 </template>
 
 <script>
 export default {
-  namme: 'users',
-  data() {
-    return {
-      query:'',
-    }
-  },
+	namme: 'users',
+	data() {
+		return {
+			query: '',
+			pagenum: 1,
+			pagesize: 5,
+			tableData: [
+				{
+					date: '2016-05-02',
+					name: '王小虎',
+					address: 'xuyujun_by@163.com'
+				},
+				{
+					date: '2016-05-04',
+					name: '刘晓萌',
+					address: 'xiaomen_by@163.com'
+				},
+				{
+					date: '2016-05-01',
+					name: '李大锤',
+					address: 'dachui_by@163.com'
+				}
+			]
+		}
+	},
+	methods: {
+		async getUserList() {
+			const { data: res } = await this.$axios.get('users', {
+				params: {
+					query: this.query,
+					pagenum: this.pagenum,
+					pagesize: this.pagesize
+				}
+			})
+			console.log(res)
+		}
+	},
+	created() {
+		this.getUserList()
+	}
 }
 </script>
 
@@ -44,10 +93,10 @@ export default {
 	width: 100%;
 	height: 100%;
 }
-.breadcrumb{
-  margin-bottom: 1rem;
+.breadcrumb {
+	margin-bottom: 1rem;
 }
-.input{
-  width: 60%;
+.input {
+	width: 60%;
 }
 </style>
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,6 +4,9 @@ const Myaxios = {}
 
 Myaxios.install = (Vue) =>{
 
+  const AUTH_TOKEN = localStorage.getItem('token') // 将获取的token令牌赋值给默认全局请求头
+  axios.defaults.headers.common['Authorization'] = AUTH_TOKEN // common[ ] 内写api文档提供字段Authorization即权限验证
+  
   axios.defaults.baseURL = 'http://39.108.193.251:8811/api/private/v1/'
 
   Vue.prototype.$axios = axios
