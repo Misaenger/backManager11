615502394e4037f310d3fc75135ad95390290ba3
yujun <2785537249@qq.com>
2019-11-11 04:55:00 +0800

    上传图片

--- src/components/goods/goodsadd.vue
+++ src/components/goods/goodsadd.vue
@@ -18,7 +18,7 @@
     <el-form label-position="top" :model="form">
 
       <!-- tab栏 -->
-      <el-tabs v-model="step" tab-position="left" style="height:800px;">
+      <el-tabs v-model="step" tab-position="left" height="300px" @tab-click="tabChange()">
         <el-tab-pane name="0" label="基本信息">
 
           <el-form-item label="商品名称">
@@ -38,77 +38,151 @@
           </el-form-item>
 
         </el-tab-pane>
-        <el-tab-pane name="1" label="商品参数">
+        <el-tab-pane name="1" label="商品参数" max-height="250">
+
+          <el-form-item :label="item.attr_name" v-for="(item,i) in arrDyparams" :key="i">
+            <el-checkbox-group v-model="checkboxGroup">
+              <el-checkbox-button :label="item1" v-for="(item1,i) in item.attr_vals.trim().split(',')" :key="i" style="margin:10px 6px"> </el-checkbox-button>
+            </el-checkbox-group>
+          </el-form-item>
+          <div v-if="show" style="margin:10px 6px">此分类暂时无参数</div>
 
         </el-tab-pane>
-        <el-tab-pane name="2" label="商品属性">
+        <el-tab-pane name="2" label="商品属性" style="overflow:auto;height:900px">
+
+          <el-form-item :label="item.attr_name" v-for="(item,i) in arrStaticparams" :key="i">
+            <el-input v-model="item.attr_vals"></el-input>
+          </el-form-item>
+          <div v-if="arrStaticparams.length<1" style="margin:10px 6px">此分类暂时无属性</div>
 
         </el-tab-pane>
         <el-tab-pane name="3" label="商品图片">
 
+          <el-form-item>
+            <el-upload class="upload-demo" action="http://39.108.193.251:8811/api/private/v1/upload" :headers="headers"
+            :on-preview="handlePreview" 
+            :on-remove="handleRemove" 
+            :on-success="handleSuccess" 
+            list-type="picture" 
+            style="width:600px">
+              <el-button size="small" type="primary">点击上传</el-button>
+              <span slot="tip" style="margin-left:12px">只能上传jpg/png文件，且不超过500kb</span>
+            </el-upload>
+          </el-form-item>
+
         </el-tab-pane>
         <el-tab-pane name="4" label="商品内容">
-                <el-button type="primary" @click="step='5'">主要按钮</el-button>
+          <el-button type="primary" @click="step='5'">主要按钮</el-button>
         </el-tab-pane>
       </el-tabs>
     </el-form>
-
-    <!-- <el-form-item label="名称">
-        <el-input v-model="formLabelAlign.name"></el-input>
-      </el-form-item>
-      <el-form-item label="活动区域">
-        <el-input v-model="formLabelAlign.region"></el-input>
-      </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input v-model="formLabelAlign.type"></el-input>
-      </el-form-item> -->
 
   </el-card>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      step: 0,
-      form: {
-        goods_name: '',
-        goods_cat: '',
-        goods_price: '',
-        goods_number: '',
-        goods_weight: '',
-        goods_introduce: '',
-        pics: '',
-        attrs: ''
-      },
-      //级联选择器
-      options: [],
-      selectedOptions: [],
-      defaultprops: {
-        value: 'cat_id',
-        label: 'cat_name',
-        children: 'children'
+	data() {
+		return {
+			step: 0,
+			form: {
+				goods_name: '',
+				goods_cat: '',
+				goods_price: '',
+				goods_number: '',
+				goods_weight: '',
+				goods_introduce: '',
+				pics: '',
+				attrs: ''
+			},
+			//级联选择器
+			options: [],
+			selectedOptions: [],
+			defaultprops: {
+				value: 'cat_id',
+				label: 'cat_name',
+				children: 'children'
+			},
+			// 商品参数
+			arrDyparams: [],
+			checkboxGroup: [],
+			show: false,
+			//商品属性
+			arrStaticparams: [],
+			//上传图片
+      headers:{
+        Authorization:localStorage.getItem('token')
       }
-    }
-  },
-  methods: {
-    handleChange() {
-      this.form.goods_cat = this.selectedOptions.join(',') // 请求参数goods_cat 有格式要求
+		}
+	},
+	methods: {
+		handleChange() {
+			this.form.goods_cat = this.selectedOptions.join(',') // 请求参数goods_cat 有格式要求
+		},
+		async getgoodcate() {
+			const res = await this.$axios.get('categories', { type: [1, 2, 3] })
+			this.options = res.data.data
+		},
+		//参数块
+		async tabChange() {
+			// 切换tab时触发
+			if (this.step === '1') {
+				// 点击tab2时触发
+				if (!this.form.goods_cat) {
+					this.$notify({
+						title: '基本信息',
+						message: '请先选择商品分类',
+						type: 'error'
+          })
+          return
+				}
+				// 发送商品分类参数，获取信息
+				const res = await this.$axios.get(
+					`categories/${this.selectedOptions[2]}/attributes?sel=many`
+				)
+				this.arrDyparams = res.data.data
+				if (res.data.data.length===0) {
+					this.show = true
+				} else {
+					this.show = false
+				}
+			} else if (this.step === '2') {
+				if (!this.form.goods_cat) {
+					this.$notify({
+						title: '基本信息',
+						message: '请先选择商品分类',
+						type: 'error'
+          })
+          return
+				}
+				// 获取商品参数
+				const res = await this.$axios.get(
+					`categories/${this.selectedOptions[2]}/attributes?sel=only`
+				)
+				this.arrStaticparams = res.data.data
+			}else if (this.step === '3') {
+				// 获取图片上传路径
+			}
+		},
+		// 上传图片
+    handleRemove(file) { // 移除图片
+    // file.response.tmp_path 是图片临时移除的路径
+		},
+		handlePreview(file) { // 预览
     },
-    async getgoodcate() {
-      const res = await this.$axios.get('categories', { type: [1, 2, 3] })
-      this.options = res.data.data
+    handleSuccess(file){ // 上传成功
+      // file.data.tmp_path是图片临时上传的路径
     }
-  },
-  created() {
-    this.getgoodcate()
-  }
+	},
+	created() {
+		this.getgoodcate()
+	}
 }
 </script>
 
 <style>
 .mybread {
-  margin: 12px 0;
-  font-size: 12px;
+	margin: 12px 0;
+	font-size: 12px;
 }
 </style>
